e40f931dca33c117679f44b5517cfc118c765f4a
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2020-02-27 17:36:24 +0100

    Remove anki-editor, I use emacs-anywhere for that now

--- packages.el
+++ packages.el
@@ -1,7 +1,6 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ~/.config/doom/packages.el
 
-(package! anki-editor)
 (package! fast-scroll)
 (package! hercules :recipe (:host gitlab :repo "jjzmajic/hercules.el"))
 (package! org-toggl :recipe (:host github :repo "mbork/org-toggl"))
