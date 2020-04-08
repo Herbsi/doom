9342d3474a72d6914051fea660b71766e5badc09
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2020-04-08 18:04:07 +0200

    Smaller font

--- config.el
+++ config.el
@@ -7,13 +7,12 @@
 (setq user-full-name "Herwig Hoehenberger"
       user-mail-address "herwig.hoehenberger@gmail.com"
       epa-file-encrypt-to user-mail-address)
-(server-start)
 
 
 ;;; UI
 (setq column-number-mode nil
       display-line-numbers-type nil
-      doom-font (font-spec :family "Fira Code" :size 16.0)
+      doom-font (font-spec :family "Fira Code" :size 11.0)
       doom-theme 'zenburn
       evil-echo-state nil
       frame-title-format "%b"
