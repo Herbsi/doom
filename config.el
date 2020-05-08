d318433eced8e8561e56b4e87238ad45de147416
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2020-05-08 20:00:06 +0200

    More config.org

--- config.el
+++ config.el
@@ -3,48 +3,12 @@
 (load! "+bindings")
 (load! "+org")
 
-;;; General Configuration
-(setq user-full-name "Herwig Hoehenberger"
-      user-mail-address "herwig.hoehenberger@gmail.com"
-      epa-file-encrypt-to user-mail-address)
-
-
-;;; UI
-(setq column-number-mode nil
-      display-line-numbers-type nil
-      doom-font (font-spec :family "Fira Code" :size (if IS-MAC 14.0 11.0))
-      doom-theme 'doom-monokai-spectrum
-      evil-echo-state nil
-      frame-title-format "%b"
-      lsp-ui-sideline-enable nil
-      mode-line-default-help-echo nil
-      scroll-bar-mode nil
-      show-help-function nil
-      show-paren-delay 0
-      uniquify-buffer-name-style 'forward
-      window-combination-resize t
-      x-stretch-cursor t
-      +ivy-buffer-preview t)
-
-
 ;;; Emacs Calc, poor man's Mathematica
 (after! calc
   (setq calc-angle-mode 'rad
         calc-algebraic-mode t
         calc-symbolic-mode t)
   (evil-set-initial-state 'calc-mode 'emacs))
-
-
-;;; Text & Input
-(setq sentence-end-double-space t       ; the only correct choice
-      default-input-method "german-postfix"
-      undo-limit 80000000
-      evil-want-fine-undo t
-      auto-save-default t
-      inhibit-compacting-font-caches t)
-(delete-selection-mode 1)
-(global-subword-mode)
-
 
 ;;; Packages
 ;; Company
@@ -54,14 +18,6 @@
 
 ;; Direnv
 (setq direnv-always-show-summary nil)
-
-
-;; Evil
-(after! evil
-  (setq evil-split-window-below t
-        evil-vsplit-window-right t
-        evil-want-fine-undo t
-        +evil-want-o/O-to-continue-comments nil))
 
 (after! evil-tex
   (add-hook! latex-mode #'evil-tex-mode))
