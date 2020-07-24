9edc953743dc4fdc69b0acb5fa911f748748ebe1
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2020-07-24 11:31:46 +0200

    (setq calc-multiplication-has-predence nil)
    It's the correct setting, in my opinion.

--- +packages.el
+++ +packages.el
@@ -4,7 +4,8 @@
 (after! calc
   (setq calc-angle-mode 'rad
         calc-algebraic-mode t
-        calc-symbolic-mode t)
+        calc-symbolic-mode t
+        calc-multiplication-has-precedence nil) ; so that a/b*c == (a/b)*c
   (evil-set-initial-state 'calc-mode 'emacs))
 
 ;; Company
