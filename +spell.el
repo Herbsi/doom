2d5ccb58ccf5df1265398ce2987c7bda726851ee
Herwig Hoehenberger <herwig.hoehenberger@gmail.com>
2021-05-06 11:19:49 +0200

    Fix ispell-personal-dictionary

--- +spell.el
+++ +spell.el
@@ -2,7 +2,7 @@
 
 (after! ispell
   (setq ispell-dictionary "en_US,de_AT"
-        ispell-personal-dictionary (concat (getenv "XDG_CONFIG_HOME") "/personal_dictionary.pws"))
+        ispell-personal-dictionary (expand-file-name "aspell/personal.pws" (getenv "XDG_CONFIG_HOME")))
   ;; ispell-set-spellchecker-params has to be called
   ;; before ispell-hunspell-add-multi-dic will work
   (ispell-set-spellchecker-params)
